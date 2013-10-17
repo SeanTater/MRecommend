9d80b72817fdcb2f7943720ce14a2853527a13a7
Sean Gallagher <stgallag@gmail.com>
2013-10-17 05:03:17 -0700

    consolidate link code

--- MRecommend/movies_search_display.cs
+++ MRecommend/movies_search_display.cs
@@ -30,17 +30,17 @@ namespace movies_database_homepage
         private List<Slot> display_slots;
 
 
-        public void display_image_n_desp(int slot_index)
+        private void display_image_n_desp(Slot slot)
         {
-            current = display_slots[slot_index];
+            current = slot;
             byte[] image_bytes = (byte[])current.movie[5];
             MemoryStream ms = new MemoryStream(image_bytes);
             System.Drawing.Bitmap img = new System.Drawing.Bitmap(ms);
-            pictureBox1.Image = img;
-            pictureBox1.Visible = true;
-            label1.Text = (String)current.movie[4];
-            label1.Visible = true;
-            button1.Visible = true;
+            cover_image.Image = img;
+            cover_image.Visible = true;
+            current_title.Text = (String)current.movie[4];
+            current_title.Visible = true;
+            view_details_button.Visible = true;
         }
         public void display_details(int slot_index)
         {
@@ -48,7 +48,7 @@ namespace movies_database_homepage
             slot.label.Visible = true;
             slot.label.Text = (String)slot.movie[1] + " (" + slot.movie[2] + ")";
             if (slot_index == 0)
-                display_image_n_desp(slot_index);
+                display_image_n_desp(slot);
         }
 
         public movies_search_display(int search_type=0, String search_data="")
@@ -58,7 +58,7 @@ namespace movies_database_homepage
             InitializeComponent();
         }
 
-        private void Form3_Load(object sender, EventArgs e)
+        private void init(object sender, EventArgs e)
         {
             display_slots = new List<Slot>() {
                 new Slot(display_slot_00, null),
@@ -72,122 +72,69 @@ namespace movies_database_homepage
                 new Slot(display_slot_08, null),
                 new Slot(display_slot_09, null)
             };
-            try 
-            {
-                String sql;
-                foreach (Slot slot in display_slots) {
-                    slot.label.Visible = false;
-                }
-                pictureBox1.Visible = false;
-                label1.Visible = false;
-                button1.Visible = false;
-                DataRowCollection movies;
-                if (search_type == 0) {
-                    form_title.Text = String.Format("Search for movies named {0}", searched_data);
-                    movies = Util.query(String.Format("SELECT * FROM `movie` WHERE Title LIKE '%{0}%'", searched_data), "movie_by_names").Rows;
+            String sql;
+            foreach (Slot slot in display_slots) {
+                slot.label.Visible = false;
+                slot.label.Links[0].Tag = slot;
+            }
+            cover_image.Visible = false;
+            current_title.Visible = false;
+            view_details_button.Visible = false;
+            DataRowCollection movies;
+            String[] name = new String[2];
+            if (search_type == 0) {
+                form_title.Text = String.Format("Search for movies named {0}", searched_data);
+                movies = Util.query(String.Format("SELECT * FROM `movie` WHERE Title LIKE '%{0}%'", searched_data), "movie_by_names").Rows;
                         
-                } else if (search_type == 1) {
-                    form_title.Text = String.Format("Search for movies with {0}", searched_data);
-                    String[] name=new String[2];
-                    name = searched_data.Split(' ');
-                    if (name.Length == 2)
-                    {
-                        sql = String.Format("SELECT DISTINCT m.* FROM person p,actor a, movie m WHERE p.Fname LIKE '%{0}%' AND p.Lname LIKE '%{1}%' AND p.SSN=a.SSN AND a.filmID=m.filmID", name[0], name[1]);
-                    }
-                    else 
-                    {
-                        sql = String.Format("SELECT DISTINCT m.* FROM person p,actor a,movie m WHERE (p.Fname LIKE '%{0}%' AND p.SSN=a.SSN AND a.filmID=m.filmID)OR(p.Lname LIKE '%{0}%' AND p.SSN=a.SSN AND a.filmID=m.filmID)", name[0]);
-                    }
-                    movies = Util.query(sql, "movie_by_actors").Rows; 
-                } else {
-                    form_title.Text = String.Format("Search for movies directed by {0}", searched_data);
-                    String[] name1=new String[2];
-                    name1 = searched_data.Split(' ');
-                    if (name1.Length == 2)
-                    {
-                        sql = String.Format("SELECT DISTINCT m.* FROM person p,director d, movie m WHERE p.Fname LIKE '%{0}%' AND p.Lname LIKE '%{1}%' AND p.SSN=d.SSN AND d.filmID=m.filmID", name1[0], name1[1]);
-                    }
-                    else 
-                    {
-                        sql = String.Format("SELECT DISTINCT m.* FROM person p,director a,movie m WHERE (p.Fname LIKE '%{0}%' AND p.SSN=d.SSN AND d.filmID=m.filmID)OR(p.Lname LIKE '%{0}%' AND p.SSN=d.SSN AND d.filmID=m.filmID)", name1[0]);
-                    }
-                    movies = Util.query(sql, "movie_by_directors").Rows;
-                }
-                if (movies.Count == 0)
+            } else if (search_type == 1) {
+                form_title.Text = String.Format("Search for movies with {0}", searched_data);
+                name = searched_data.Split(' ');
+                if (name.Length == 2)
                 {
-                    MessageBox.Show("Your search did not match any movies.");
-                    this.Close();
+                    sql = String.Format("SELECT DISTINCT m.* FROM person p,actor a, movie m WHERE p.Fname LIKE '%{0}%' AND p.Lname LIKE '%{1}%' AND p.SSN=a.SSN AND a.filmID=m.filmID", name[0], name[1]);
                 }
-                else
+                else 
                 {
-                    for (int i = 0; i < 10 && i < movies.Count; i++)
-                    {
-                        display_slots[i].movie = movies[i];
-                        display_details(i);
-                    }
+                    sql = String.Format("SELECT DISTINCT m.* FROM person p,actor a,movie m WHERE (p.Fname LIKE '%{0}%' AND p.SSN=a.SSN AND a.filmID=m.filmID)OR(p.Lname LIKE '%{0}%' AND p.SSN=a.SSN AND a.filmID=m.filmID)", name[0]);
                 }
+                movies = Util.query(sql, "movie_by_actors").Rows; 
+            } else {
+                form_title.Text = String.Format("Search for movies directed by {0}", searched_data);
+                name = searched_data.Split(' ');
+                if (name.Length == 2)
+                {
+                    sql = String.Format("SELECT DISTINCT m.* FROM person p,director d, movie m WHERE p.Fname LIKE '%{0}%' AND p.Lname LIKE '%{1}%' AND p.SSN=d.SSN AND d.filmID=m.filmID", name[0], name[1]);
+                }
+                else 
+                {
+                    sql = String.Format("SELECT DISTINCT m.* FROM person p,director a,movie m WHERE (p.Fname LIKE '%{0}%' AND p.SSN=d.SSN AND d.filmID=m.filmID)OR(p.Lname LIKE '%{0}%' AND p.SSN=d.SSN AND d.filmID=m.filmID)", name[0]);
+                }
+                movies = Util.query(sql, "movie_by_directors").Rows;
             }
-            catch(Exception ex)
+            if (movies.Count == 0)
             {
-                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Your search did not match any movies.");
+                this.Close();
             }
-           
+            else
+            {
+                for (int i = 0; i < 10 && i < movies.Count; i++)
+                {
+                    display_slots[i].movie = movies[i];
+                    display_details(i);
+                }
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void view_current_movie_details(object sender, EventArgs e)
         {
             Movies.Movie_HomePage mh = new Movies.Movie_HomePage(current.movie[0].ToString(),"0000000");
             mh.Show();
         }
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void slot_clicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            display_image_n_desp(0);
-        }
-
-        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            display_image_n_desp(1);
-        }
-
-        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            display_image_n_desp(3);
-        }
-
-        private void linkLabel7_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            display_image_n_desp(6);
-        }
-
-        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            display_image_n_desp(2);
-        }
-
-        private void linkLabel6_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            display_image_n_desp(5);
-        }
-
-        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            display_image_n_desp(4);
-        }
-
-        private void linkLabel8_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            display_image_n_desp(7);
-        }
-
-        private void linkLabel9_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            display_image_n_desp(8);
-        }
-
-        private void linkLabel10_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            display_image_n_desp(9);
+            display_image_n_desp((Slot)e.Link.Tag);
         }
     }
 }
--- MRecommend/movies_search_display.designer.cs
+++ MRecommend/movies_search_display.designer.cs
@@ -39,164 +39,164 @@
             this.display_slot_07 = new System.Windows.Forms.LinkLabel();
             this.display_slot_08 = new System.Windows.Forms.LinkLabel();
             this.display_slot_09 = new System.Windows.Forms.LinkLabel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.cover_image = new System.Windows.Forms.PictureBox();
+            this.current_title = new System.Windows.Forms.Label();
+            this.view_details_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cover_image)).BeginInit();
             this.SuspendLayout();
             // 
-            // label6
+            // form_title
             // 
             this.form_title.AutoSize = true;
             this.form_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.form_title.Location = new System.Drawing.Point(36, 13);
-            this.form_title.Name = "label6";
+            this.form_title.Name = "form_title";
             this.form_title.Size = new System.Drawing.Size(86, 31);
             this.form_title.TabIndex = 3;
             this.form_title.Text = "label6";
             // 
-            // linkLabel1
+            // display_slot_00
             // 
             this.display_slot_00.AutoSize = true;
             this.display_slot_00.Location = new System.Drawing.Point(64, 88);
-            this.display_slot_00.Name = "linkLabel1";
+            this.display_slot_00.Name = "display_slot_00";
             this.display_slot_00.Size = new System.Drawing.Size(55, 13);
             this.display_slot_00.TabIndex = 4;
             this.display_slot_00.TabStop = true;
             this.display_slot_00.Text = "linkLabel1";
-            this.display_slot_00.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.display_slot_00.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // linkLabel2
+            // display_slot_01
             // 
             this.display_slot_01.AutoSize = true;
             this.display_slot_01.Location = new System.Drawing.Point(64, 112);
-            this.display_slot_01.Name = "linkLabel2";
+            this.display_slot_01.Name = "display_slot_01";
             this.display_slot_01.Size = new System.Drawing.Size(55, 13);
             this.display_slot_01.TabIndex = 5;
             this.display_slot_01.TabStop = true;
             this.display_slot_01.Text = "linkLabel2";
-            this.display_slot_01.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            this.display_slot_01.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // linkLabel3
+            // display_slot_02
             // 
             this.display_slot_02.AutoSize = true;
             this.display_slot_02.Location = new System.Drawing.Point(64, 140);
-            this.display_slot_02.Name = "linkLabel3";
+            this.display_slot_02.Name = "display_slot_02";
             this.display_slot_02.Size = new System.Drawing.Size(55, 13);
             this.display_slot_02.TabIndex = 6;
             this.display_slot_02.TabStop = true;
             this.display_slot_02.Text = "linkLabel3";
-            this.display_slot_02.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            this.display_slot_02.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // linkLabel4
+            // display_slot_03
             // 
             this.display_slot_03.AutoSize = true;
             this.display_slot_03.Location = new System.Drawing.Point(64, 167);
-            this.display_slot_03.Name = "linkLabel4";
+            this.display_slot_03.Name = "display_slot_03";
             this.display_slot_03.Size = new System.Drawing.Size(55, 13);
             this.display_slot_03.TabIndex = 7;
             this.display_slot_03.TabStop = true;
             this.display_slot_03.Text = "linkLabel4";
-            this.display_slot_03.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            this.display_slot_03.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // linkLabel5
+            // display_slot_04
             // 
             this.display_slot_04.AutoSize = true;
             this.display_slot_04.Location = new System.Drawing.Point(64, 194);
-            this.display_slot_04.Name = "linkLabel5";
+            this.display_slot_04.Name = "display_slot_04";
             this.display_slot_04.Size = new System.Drawing.Size(55, 13);
             this.display_slot_04.TabIndex = 8;
             this.display_slot_04.TabStop = true;
             this.display_slot_04.Text = "linkLabel5";
             this.display_slot_04.UseWaitCursor = true;
-            this.display_slot_04.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            this.display_slot_04.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // linkLabel6
+            // display_slot_05
             // 
             this.display_slot_05.AutoSize = true;
             this.display_slot_05.Location = new System.Drawing.Point(64, 221);
-            this.display_slot_05.Name = "linkLabel6";
+            this.display_slot_05.Name = "display_slot_05";
             this.display_slot_05.Size = new System.Drawing.Size(55, 13);
             this.display_slot_05.TabIndex = 9;
             this.display_slot_05.TabStop = true;
             this.display_slot_05.Text = "linkLabel6";
-            this.display_slot_05.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel6_LinkClicked);
+            this.display_slot_05.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // linkLabel7
+            // display_slot_06
             // 
             this.display_slot_06.AutoSize = true;
             this.display_slot_06.Location = new System.Drawing.Point(64, 250);
-            this.display_slot_06.Name = "linkLabel7";
+            this.display_slot_06.Name = "display_slot_06";
             this.display_slot_06.Size = new System.Drawing.Size(55, 13);
             this.display_slot_06.TabIndex = 10;
             this.display_slot_06.TabStop = true;
             this.display_slot_06.Text = "linkLabel7";
-            this.display_slot_06.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel7_LinkClicked);
+            this.display_slot_06.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // linkLabel8
+            // display_slot_07
             // 
             this.display_slot_07.AutoSize = true;
             this.display_slot_07.Location = new System.Drawing.Point(64, 282);
-            this.display_slot_07.Name = "linkLabel8";
+            this.display_slot_07.Name = "display_slot_07";
             this.display_slot_07.Size = new System.Drawing.Size(55, 13);
             this.display_slot_07.TabIndex = 11;
             this.display_slot_07.TabStop = true;
             this.display_slot_07.Text = "linkLabel8";
-            this.display_slot_07.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel8_LinkClicked);
+            this.display_slot_07.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // linkLabel9
+            // display_slot_08
             // 
             this.display_slot_08.AutoSize = true;
             this.display_slot_08.Location = new System.Drawing.Point(64, 310);
-            this.display_slot_08.Name = "linkLabel9";
+            this.display_slot_08.Name = "display_slot_08";
             this.display_slot_08.Size = new System.Drawing.Size(55, 13);
             this.display_slot_08.TabIndex = 12;
             this.display_slot_08.TabStop = true;
             this.display_slot_08.Text = "linkLabel9";
-            this.display_slot_08.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel9_LinkClicked);
+            this.display_slot_08.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // linkLabel10
+            // display_slot_09
             // 
             this.display_slot_09.AutoSize = true;
             this.display_slot_09.Location = new System.Drawing.Point(64, 336);
-            this.display_slot_09.Name = "linkLabel10";
+            this.display_slot_09.Name = "display_slot_09";
             this.display_slot_09.Size = new System.Drawing.Size(61, 13);
             this.display_slot_09.TabIndex = 13;
             this.display_slot_09.TabStop = true;
             this.display_slot_09.Text = "linkLabel10";
-            this.display_slot_09.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel10_LinkClicked);
+            this.display_slot_09.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.slot_clicked);
             // 
-            // pictureBox1
+            // cover_image
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(299, 74);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(295, 275);
-            this.pictureBox1.TabIndex = 14;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Visible = false;
+            this.cover_image.Location = new System.Drawing.Point(299, 74);
+            this.cover_image.Name = "cover_image";
+            this.cover_image.Size = new System.Drawing.Size(295, 275);
+            this.cover_image.TabIndex = 14;
+            this.cover_image.TabStop = false;
+            this.cover_image.Visible = false;
             // 
-            // label1
+            // current_title
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(670, 74);
-            this.label1.MaximumSize = new System.Drawing.Size(200, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
-            this.label1.TabIndex = 15;
-            this.label1.Text = "label1";
-            this.label1.Visible = false;
+            this.current_title.AutoSize = true;
+            this.current_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.current_title.Location = new System.Drawing.Point(670, 74);
+            this.current_title.MaximumSize = new System.Drawing.Size(200, 0);
+            this.current_title.Name = "current_title";
+            this.current_title.Size = new System.Drawing.Size(51, 20);
+            this.current_title.TabIndex = 15;
+            this.current_title.Text = "label1";
+            this.current_title.Visible = false;
             // 
-            // button1
+            // view_details_button
             // 
-            this.button1.Location = new System.Drawing.Point(673, 325);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 23);
-            this.button1.TabIndex = 16;
-            this.button1.Text = "View movie details";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.view_details_button.Location = new System.Drawing.Point(673, 325);
+            this.view_details_button.Name = "view_details_button";
+            this.view_details_button.Size = new System.Drawing.Size(130, 23);
+            this.view_details_button.TabIndex = 16;
+            this.view_details_button.Text = "View movie details";
+            this.view_details_button.UseVisualStyleBackColor = true;
+            this.view_details_button.Visible = false;
+            this.view_details_button.Click += new System.EventHandler(this.view_current_movie_details);
             // 
             // movies_search_display
             // 
@@ -204,9 +204,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CausesValidation = false;
             this.ClientSize = new System.Drawing.Size(984, 512);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.view_details_button);
+            this.Controls.Add(this.current_title);
+            this.Controls.Add(this.cover_image);
             this.Controls.Add(this.display_slot_09);
             this.Controls.Add(this.display_slot_08);
             this.Controls.Add(this.display_slot_07);
@@ -220,8 +220,8 @@
             this.Controls.Add(this.form_title);
             this.Name = "movies_search_display";
             this.Text = "movies search page";
-            this.Load += new System.EventHandler(this.Form3_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.init);
+            ((System.ComponentModel.ISupportInitialize)(this.cover_image)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -240,9 +240,9 @@
         private System.Windows.Forms.LinkLabel display_slot_07;
         private System.Windows.Forms.LinkLabel display_slot_08;
         private System.Windows.Forms.LinkLabel display_slot_09;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox cover_image;
+        private System.Windows.Forms.Label current_title;
+        private System.Windows.Forms.Button view_details_button;
 
 
     }
