859b152dc62ca1b7e558107ebf955d176a5e4b37
Sean Gallagher <stgallag@gmail.com>
2013-10-17 03:44:47 -0700

    movies_search_display seems more organized

--- MRecommend/movies_search_display.cs
+++ MRecommend/movies_search_display.cs
@@ -15,36 +15,40 @@ namespace movies_database_homepage
 {
     public partial class movies_search_display : Form
     {
-        public DataRow current;
-        public int search_type;
-        public String searched_data;
-        public MySqlDataAdapter my_data_adapter;
-        public DataSet my_dataset;
-        public DataTable my_data_table;
-        private List<LinkLabel> link_labels;
-
-
-        public void display_image_n_desp(int count)
+        private Slot current;
+        private int search_type;
+        private String searched_data;
+        private class Slot
         {
-            
-            DataRow dr_new = my_data_table.Rows[count];
-            current = dr_new;
-            byte[] image_bytes = (byte[])dr_new[5];
+            public Slot(LinkLabel l, DataRow m) {
+                label = l;
+                movie = m;
+            }
+            public LinkLabel label;
+            public DataRow movie;
+        }
+        private List<Slot> display_slots;
+
+
+        public void display_image_n_desp(int slot_index)
+        {
+            current = display_slots[slot_index];
+            byte[] image_bytes = (byte[])current.movie[5];
             MemoryStream ms = new MemoryStream(image_bytes);
             System.Drawing.Bitmap img = new System.Drawing.Bitmap(ms);
             pictureBox1.Image = img;
             pictureBox1.Visible = true;
-            label1.Text = (String)dr_new[4];
+            label1.Text = (String)current.movie[4];
             label1.Visible = true;
             button1.Visible = true;
         }
-        public void display_details(DataTable data_table, int count)
+        public void display_details(int slot_index)
         {
-            DataRow movie = data_table.Rows[count];
-            link_labels[count].Visible = true;
-            link_labels[count].Text = (String)movie[1] + " (" + movie[2] + ")";
-            if (count == 0)
-                display_image_n_desp(0);
+            Slot slot = display_slots[slot_index];
+            slot.label.Visible = true;
+            slot.label.Text = (String)slot.movie[1] + " (" + slot.movie[2] + ")";
+            if (slot_index == 0)
+                display_image_n_desp(slot_index);
         }
 
         public movies_search_display(int search_type=0, String search_data="")
@@ -56,125 +60,73 @@ namespace movies_database_homepage
 
         private void Form3_Load(object sender, EventArgs e)
         {
-            link_labels = new List<LinkLabel>() {
-                linkLabel1,
-                linkLabel2,
-                linkLabel3,
-                linkLabel4,
-                linkLabel5,
-                linkLabel6,
-                linkLabel7,
-                linkLabel8,
-                linkLabel9,
-                linkLabel10
+            display_slots = new List<Slot>() {
+                new Slot(linkLabel1, null),
+                new Slot(linkLabel2, null),
+                new Slot(linkLabel3, null),
+                new Slot(linkLabel4, null),
+                new Slot(linkLabel5, null),
+                new Slot(linkLabel6, null),
+                new Slot(linkLabel7, null),
+                new Slot(linkLabel8, null),
+                new Slot(linkLabel9, null),
+                new Slot(linkLabel10, null)
             };
             try 
             {
                 String sql;
-                foreach (LinkLabel label in link_labels) {
-                    label.Visible = false;
+                foreach (Slot slot in display_slots) {
+                    slot.label.Visible = false;
                 }
                 pictureBox1.Visible = false;
                 label1.Visible = false;
                 button1.Visible = false;
-                switch (search_type)
+                DataRowCollection movies;
+                if (search_type == 0) {
+                    label6.Text = "Movies";
+                    movies = Util.query("SELECT * FROM `movie` WHERE Title='" + searched_data + "'", "movie_by_names").Rows;
+                        
+                } else if (search_type == 1) {
+                    label6.Text = "Actors";
+                    String[] name=new String[2];
+                    name = searched_data.Split(' ');
+                    if (name.Length == 2)
+                    {
+                        sql = "SELECT DISTINCT m.* FROM person p,actor a, movie m WHERE p.Fname='" + name[0] + "' AND p.Lname='" + name[1] + "' AND p.SSN=a.SSN AND a.filmID=m.filmID";
+                    }
+                    else 
+                    {
+                        sql = "SELECT DISTINCT m.* FROM person p,actor a,movie m WHERE (p.Fname='" + name[0] + "' AND p.SSN=a.SSN AND a.filmID=m.filmID)OR(p.Lname='" + name[0] + "' AND p.SSN=a.SSN AND a.filmID=m.filmID)";
+                    }
+                    movies = Util.query(sql, "movie_by_actors").Rows; 
+                } else {
+                    label6.Text = "Directors";
+                    String[] name1=new String[2];
+                    name1 = searched_data.Split(' ');
+                    if (name1.Length == 2)
+                    {
+                        sql = "SELECT DISTINCT m.* FROM person p,director d, movie m WHERE p.Fname='" + name1[0] + "' AND p.Lname='" + name1[1] + "' AND p.SSN=d.SSN AND d.filmID=m.filmID";
+                    }
+                    else 
+                    {
+                        sql = "SELECT DISTINCT m.* FROM person p,director a,movie m WHERE (p.Fname='" + name1[0] + "' AND p.SSN=d.SSN AND d.filmID=m.filmID)OR(p.Lname='" + name1[0] + "' AND p.SSN=d.SSN AND d.filmID=m.filmID)";
+                    }
+                    movies = Util.query(sql, "movie_by_directors").Rows;
+                }
+                if (movies.Count == 0)
                 {
-                    case 0:
-                        label6.Text = "Movies";
-                        sql = "SELECT * FROM `movie` WHERE Title='" + searched_data + "'";
-                        int cnt = 0;
-                        my_data_adapter = new MySqlDataAdapter(sql, Util.connect());
-                        my_dataset = new DataSet();
-                        my_data_table = new DataTable();
-                        my_data_adapter.Fill(my_dataset, "movie_by_names");
-                        my_data_table = my_dataset.Tables["movie_by_names"];
-                        if (my_data_table.Rows.Count == 0)
-                        {
-                            MessageBox.Show("no movies for the given name exist");
-                            movies_database_homepage.mainpage mp = new mainpage();
-                            mp.Show();
-                            this.Hide();
-                        }
-                        else 
-                        {
-                            while ((my_data_table.Rows.Count - cnt) > 0)
-                            {
-                                display_details(my_data_table, cnt);
-                                cnt++;
-                            }
-                        }
-                        
-                        break;
-                    case 1:
-                        label6.Text = "Actors";
-                        int cnt1 = 0;
-                        String[] name=new String[2];
-                        name = searched_data.Split(' ');
-                        if (name.Length == 2)
-                        {
-                            sql = "SELECT DISTINCT m.* FROM person p,actor a, movie m WHERE p.Fname='" + name[0] + "' AND p.Lname='" + name[1] + "' AND p.SSN=a.SSN AND a.filmID=m.filmID";
-                        }
-                        else 
-                        {
-                            sql = "SELECT DISTINCT m.* FROM person p,actor a,movie m WHERE (p.Fname='" + name[0] + "' AND p.SSN=a.SSN AND a.filmID=m.filmID)OR(p.Lname='" + name[0] + "' AND p.SSN=a.SSN AND a.filmID=m.filmID)";
-                        }
-                        my_data_adapter = new MySqlDataAdapter(sql, Util.connect());
-                        my_dataset = new DataSet();
-                        my_data_table = new DataTable();
-                        my_data_adapter.Fill(my_dataset, "movie_by_actors");
-                        my_data_table = my_dataset.Tables["movie_by_actors"];
-                        int count = my_data_table.Rows.Count;
-                        if (count == 0)
-                        {
-                            MessageBox.Show("no actors for the given name exist");
-                            movies_database_homepage.mainpage mp = new mainpage();
-                            mp.Show();
-                            this.Hide();
-                        }
-                        else 
-                        {
-                            while ((my_data_table.Rows.Count - cnt1) > 0)
-                            {
-                                display_details(my_data_table, cnt1);
-                                cnt1++;
-                            }
-                        }
-                        
-                        break;
-                    case 2:
-                        label6.Text = "Directors";
-                        int cnt2 = 0;
-                        String[] name1=new String[2];
-                        name1 = searched_data.Split(' ');
-                        if (name1.Length == 2)
-                        {
-                            sql = "SELECT DISTINCT m.* FROM person p,director d, movie m WHERE p.Fname='" + name1[0] + "' AND p.Lname='" + name1[1] + "' AND p.SSN=d.SSN AND d.filmID=m.filmID";
-                        }
-                        else 
-                        {
-                            sql = "SELECT DISTINCT m.* FROM person p,director a,movie m WHERE (p.Fname='" + name1[0] + "' AND p.SSN=d.SSN AND d.filmID=m.filmID)OR(p.Lname='" + name1[0] + "' AND p.SSN=d.SSN AND d.filmID=m.filmID)";
-                        }
-                        my_data_adapter = new MySqlDataAdapter(sql, Util.connect());
-                        my_dataset = new DataSet();
-                        my_data_table = new DataTable();
-                        my_data_adapter.Fill(my_dataset, "movie_by_directors");
-                        my_data_table = my_dataset.Tables["movie_by_directors"];
-                        if (my_data_table.Rows.Count == 0)
-                        {
-                            MessageBox.Show("no directors for the given name exist");
-                            movies_database_homepage.mainpage mp = new mainpage();
-                            mp.Show();
-                            this.Hide();
-                        }
-                        else
-                        {
-                            while ((my_data_table.Rows.Count - cnt2) > 0)
-                            {
-                                display_details(my_data_table, cnt2);
-                                cnt2++;
-                            }
-                        }
-                        break;
+                    MessageBox.Show("Your search did not match any movies.");
+                    movies_database_homepage.mainpage mp = new mainpage();
+                    mp.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    for (int i = 0; i < 10 && i < movies.Count; i++)
+                    {
+                        display_slots[i].movie = movies[i];
+                        display_details(i);
+                    }
                 }
             }
             catch(Exception ex)
@@ -186,7 +138,7 @@ namespace movies_database_homepage
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Movies.Movie_HomePage mh = new Movies.Movie_HomePage(current[0].ToString(),"0000000");
+            Movies.Movie_HomePage mh = new Movies.Movie_HomePage(current.movie[0].ToString(),"0000000");
             mh.Show();
         }
 
