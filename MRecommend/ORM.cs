e0ae1bc3cd748b327828069d30fb4f2eba4c217c
SeanTater <stgallag@gmail.com>
2013-11-11 13:32:45 -0800

    Make a better, almost usable ORM

--- MRecommend/ORM.cs
+++ MRecommend/ORM.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using MySql.Data.MySqlClient;
 using System.Windows.Forms; // for MessageBox
+using System.Dynamic;
 using System;
 
 namespace Movies
@@ -40,16 +41,125 @@ namespace Movies
         }
     }
 
-    public class DBObject(ORM) {
-        public string name;
-        public string column_names[];
+    abstract public class DBObject : DynamicObject
+    {
+        public static string table_name;
+        public static string[] column_names;
+        public DataRow entries;
 
-        public static DBObject query(String shortql) {
-            ORM.query(string.Format("SELECT * FROM {0} {1}", name, shortql));
+        private int column_to_index(string name)
+        {
+            name = name.ToLower();
+            for (int i = 0; i < column_names.Length; i++)
+                if (column_names[i].ToLower() == name)
+                    return i;
+            return -1;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            int index = column_to_index(binder.Name);
+            if (index < 0)
+            {
+                result = null;
+                return false;
+            }
+            else
+            {
+                result = entries[column_to_index(binder.Name)];
+                return true;
+            }
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            int index = column_to_index(binder.Name);
+            if (index < 0)
+            {
+                return false;
+            }
+            else
+            {
+                value = entries[column_to_index(binder.Name)];
+                return true;
+            }
+        }
+
+        public static DataRowCollection select(String shortql)
+        {
+            return ORM.query(string.Format("SELECT * FROM {0} {1}", table_name, shortql)).Rows;
+        }
+
+        public static int save()
+        {
+            string column_name_group = string.Join(", ", column_names);
+            return ORM.non_query(string.Format("INSERT INTO {} VALUES ({}) ON DUPLICATE KEY UPDATE;", table_name, column_name_group));
         }
     }
 
-    public class Item {
-        
+    public class Movie : DBObject 
+    {
+        public static string table_name = "movie";
+        public static string[] column_names = {"filmID", "Title", "year", "Avg_rating", "Description", "MoviePoster"};
+    }
+
+    public class Actor : DBObject
+    {
+        public static string table_name = "actor";
+        public static string[] column_names = {"SSN", "filmID"};
+    }
+
+    public class  : DBObject
+    {
+        public static string table_name = "director";
+        public static string[] column_names = { "SSN", "filmID" };
+    }
+
+    public class Genre : DBObject
+    {
+        public static string table_name = "genre";
+        public static string[] column_names = {"filmID", "genre"};
+    }
+
+    public class Like : DBObject
+    {
+        public static string table_name = "likes";
+        public static string[] column_names = {"SSN", "genre"};
+    }
+
+    public class Review: DBObject
+    {
+        public static string table_name = "movie_review";
+        public static string[] column_names = {"filmID", "SSN", "Text", "Rating"};
+    }
+
+    public class NowPlaying : DBObject
+    {
+        public static string table_name = "now_playing";
+        public static string[] column_names = {"TID", "filmID", "date", "time"};
+    }
+
+    public class Person : DBObject
+    {
+        public static string table_name = "person";
+        public static string[] column_names = {"SSN", "Fname", "Lname", "gender"};
+    }
+
+    public class Recommend : DBObject
+    {
+        public static string table_name = "recommend";
+        public static string[] column_names = {"SSN", "filmID"};
+    }
+    
+    public class Theater : DBObject
+    {
+        public static string table_name = "theater";
+        public static string[] column_names = {"TID", "Name", "City"};
+    }
+
+    public class User : DBObject
+  
+        public static string table_name = "user";
+        public static string[] column_names = {"SSN", "Username", "Password"};
     }
 }
